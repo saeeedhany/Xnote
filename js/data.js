440c55ddb763d1fc914e619eba053c7dcce4daac
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-04-20 05:46:07 +0200

    trynomology

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 window.XNOTE = window.XNOTE || {};
 
-XNOTE.whatsappNumber = '201000000000';
+XNOTE.whatsappNumber = '201027575715';
 
 /* ── Shipping costs (EGP) ──
    local   = Cairo & nearby areas
